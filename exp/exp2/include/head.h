cc9c71cd8deae93e26776470bf061bf9229afc9e
Shujakuinkuraudo <zhouyunyao1@gmail.com>
2022-09-22 08:10:42 +0800

    modify

---
+++ exp/exp2/include/head.h
@@ -0,0 +1,29 @@
+/*
+ * @Author: Shujakuinkuraudo zhouyunyao1@gmail.com
+ * @Date: 2022-09-19 19:26:37
+ * @LastEditors: Shujakuinkuraudo zhouyunyao1@gmail.com
+ * @LastEditTime: 2022-09-20 18:29:48
+ * @FilePath: /DSM/exp1/include/head.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <functional>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <set>
+#include <map>
+using namespace std;
+#define _for(i,a,b) for(int i=a;i<b;i++)
+
+
---
+++ make.sh
@@ -0,0 +1,9 @@
+#! /bin/bash 
+cp ../exp1/CMakeLists.txt ./ 
+mkdir build
+touch main.cpp
+mkdir include
+mkdir src
+cd include
+cp ../../exp1/include/head.h ./
+
